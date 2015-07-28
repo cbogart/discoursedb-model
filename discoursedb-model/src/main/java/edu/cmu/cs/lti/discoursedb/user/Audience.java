ab0365106a3fd719e4ec4d981ee41927110b0297
Oliver Ferschke <ferschke@cd.cmu.edu>
2015-07-27 21:45:42 -0400

    Updated getters/setters in Audience

--- discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/user/Audience.java
+++ discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/user/Audience.java
@@ -17,4 +17,28 @@ public class Audience implements Serializable {
 	private Set<Annotation> annotations = new HashSet<Annotation>();
 	
 	public Audience(){}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Audience_Type getType() {
+		return type;
+	}
+
+	public void setType(Audience_Type type) {
+		this.type = type;
+	}
+
+	public Set<Annotation> getAnnotations() {
+		return annotations;
+	}
+
+	public void setAnnotations(Set<Annotation> annotations) {
+		this.annotations = annotations;
+	}
 }
