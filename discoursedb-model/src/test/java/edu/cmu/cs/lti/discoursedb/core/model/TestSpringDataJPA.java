9cf683c79a4bf46a53383acd095d60b8a89617d4
ferschke <ferschke@cs.cmu.edu>
2015-07-30 15:14:30 -0400

    Made test class transactional to allow lazy loading

--- discoursedb-model/src/test/java/edu/cmu/cs/lti/discoursedb/core/model/TestSpringDataJPA.java
+++ discoursedb-model/src/test/java/edu/cmu/cs/lti/discoursedb/core/model/TestSpringDataJPA.java
@@ -1,15 +1,25 @@
 package edu.cmu.cs.lti.discoursedb.core.model;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 import edu.cmu.cs.lti.discoursedb.core.model.user.User;
 import edu.cmu.cs.lti.discoursedb.core.repository.user.UserRepository;
 
-@Configuration
+/**
+ * @author oliverf
+ * 
+ * 1. We need a componentscan in order to discover the configuration
+ * 2. We need to wrap everything in a transaction in order to allow lazy loading to work.
+ * 		If we didn't do that, than we would have a single transaction for each interaction with a data repository.
+ * 		However, the transaction does not extend to the proxy objects retrieved in lazy loading
+ *
+ */
+@Transactional 
 @ComponentScan(value = { "edu.cmu.cs.lti.discoursedb" })
 public class TestSpringDataJPA implements CommandLineRunner {
 
@@ -24,6 +34,7 @@ public class TestSpringDataJPA implements CommandLineRunner {
     public void run(String... strings) throws Exception {
 		for(User u:userRepo.findAll()){
 	    	  System.out.println(u.getRealname());
+	    	  System.out.println(u.getContentInteractions().size());
 	      }
 	}
 	
