8d82a1b5af78c3791896809c70fc7f5717fd6981
ferschke <ferschke@cs.cmu.edu>
2015-08-06 17:22:55 -0400

    Added new access method for discourse entity

--- discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/repository/macro/DiscourseRepository.java
+++ discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/repository/macro/DiscourseRepository.java
@@ -1,9 +1,16 @@
 package edu.cmu.cs.lti.discoursedb.core.repository.macro;
 
+import java.util.List;
+
 import edu.cmu.cs.lti.discoursedb.core.model.macro.Discourse;
 import edu.cmu.cs.lti.discoursedb.core.repository.CoreBaseRepository;
 
 public interface DiscourseRepository extends CoreBaseRepository<Discourse,Long>{
-    
+	
+	Discourse findOneByNameAndDescriptor(String name, String descriptor);
+	
+	List<Discourse> findByName(String name);
+
+	
     
 }
