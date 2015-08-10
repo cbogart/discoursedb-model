622f086f38aefded15780c0242b8d19ed009a604
Oliver Ferschke <oliver.ferschke@gmail.com>
2015-08-10 11:21:35 -0400

    Added new access methods to ContributionRepository

--- discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/repository/macro/ContributionRepository.java
+++ discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/repository/macro/ContributionRepository.java
@@ -1,9 +1,12 @@
 package edu.cmu.cs.lti.discoursedb.core.repository.macro;
 
+import java.util.Optional;
+
 import edu.cmu.cs.lti.discoursedb.core.model.macro.Contribution;
 import edu.cmu.cs.lti.discoursedb.core.repository.CoreBaseRepository;
 
 public interface ContributionRepository extends CoreBaseRepository<Contribution,Long>{
     
-    
+	public Optional<Contribution> findBySourceId(String id);
+
 }
