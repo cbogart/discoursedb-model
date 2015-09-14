d7aa5b60a904476bec15ae513ef314383d48f2f5
ferschke <ferschke@cs.cmu.edu>
2015-09-14 11:13:41 -0400

    Removed User list from Discourse Entity

--- discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/model/macro/Discourse.java
+++ discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/model/macro/Discourse.java
@@ -44,8 +44,6 @@ public class Discourse implements Serializable {
 
 	private Set<DiscourseToDiscoursePart> discourseToDiscourseParts = new HashSet<DiscourseToDiscoursePart>();
 
-	private Set<User> users;
-
 	public Discourse() {}
 
 	public Discourse(String name, String descriptor){
@@ -100,15 +98,6 @@ public class Discourse implements Serializable {
 
 	public void setDiscourseToDiscourseParts(Set<DiscourseToDiscoursePart> discourseToDiscourseParts) {
 		this.discourseToDiscourseParts = discourseToDiscourseParts;
-	}
-
-	@ManyToMany(fetch = FetchType.LAZY, mappedBy = "discourses")
-	public Set<User> getUsers() {
-		return users;
-	}
-
-	public void setUsers(Set<User> users) {
-		this.users = users;
 	}
 
 }
