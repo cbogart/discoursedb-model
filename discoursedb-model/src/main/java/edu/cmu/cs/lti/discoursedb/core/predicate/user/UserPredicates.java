85d319a8b532fee703a56325464ad6b3dee3ed68
ferschke <ferschke@cs.cmu.edu>
2015-09-24 18:59:38 -0400

    Updated predicates stub

--- discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/predicate/user/UserPredicates.java
+++ discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/predicate/user/UserPredicates.java
@@ -2,6 +2,16 @@ package edu.cmu.cs.lti.discoursedb.core.predicate.user;
 
 public final class UserPredicates {
 
-	private UserPredicates() {}
+	private UserPredicates() {
+	}
 
+	// create predicate that checks whether QUser.user has
+
+//	public static Predicate userHasSourceId(String sourceId) {
+//		if (sourceId == null || sourceId.isEmpty()) {
+//			return QUser.user.isNotNull();
+//		} else {
+//		}
+//		return null;
+//	}
 }
