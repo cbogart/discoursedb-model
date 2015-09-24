db202c1bf21c707374a3b32ea1a32f47d6db0a5f
ferschke <ferschke@cs.cmu.edu>
2015-09-24 18:30:45 -0400

    Added UserPredicate stub for QueryDSL

---
+++ discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/predicate/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains QueryDSL predicates for all entity classes.
+ * 
+ * @author Oliver Ferschke
+ *
+ */
+package edu.cmu.cs.lti.discoursedb.core.predicate;
---
+++ discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/predicate/user/UserPredicates.java
@@ -0,0 +1,7 @@
+package edu.cmu.cs.lti.discoursedb.core.predicate.user;
+
+public final class UserPredicates {
+
+	private UserPredicates() {}
+
+}
