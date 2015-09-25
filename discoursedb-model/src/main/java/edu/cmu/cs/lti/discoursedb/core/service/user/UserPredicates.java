e5bdd4a66a73b87dec154bf6f0e263996c541145
Oliver Ferschke <oliver.ferschke@gmail.com>
2015-09-24 21:11:35 -0400

    Added new predicate stubs

--- discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/service/user/UserPredicates.java
+++ discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/service/user/UserPredicates.java
@@ -9,12 +9,6 @@ public final class UserPredicates {
 	private UserPredicates() {
 	}
 
-	/**
-	 * Checks whether a user matches the provided sourceid in any of the datasources associated with them.
-	 * 
-	 * @param sourceId
-	 * @return
-	 */
 	public static Predicate userHasSourceId(String sourceId) {
 		if (sourceId == null || sourceId.isEmpty()) {
 			return QUser.user.isNotNull();
@@ -22,4 +16,24 @@ public final class UserPredicates {
 			return QUser.user.dataSourceAggregate.sources.any().entitySourceId.eq(sourceId);
 		}
 	}
+
+//The following predicates need to check whether the properties occur in a single DataSourceInstance 
+	
+//	public static Predicate userHasSourceIdAndDataSourceType(String sourceId, DataSourceTypes type) {
+//		if (sourceId == null || sourceId.isEmpty() || type == null) {
+//			return QUser.user.isNotNull();
+//		} else {
+//			return QUser.user.dataSourceAggregate.sources.any().entitySourceId.eq(sourceId)
+//					.and(QUser.user.dataSourceAggregate.sources.any().sourceType.eq(type));
+//		}
+//	}
+//
+//	public static Predicate userHasSourceIdAndDataSetName(String sourceId, String dataSetName) {
+//		if (sourceId == null || sourceId.isEmpty() || dataSetName == null || dataSetName.isEmpty()) {
+//			return QUser.user.isNotNull();
+//		} else {
+//			return QUser.user.dataSourceAggregate.sources.any().entitySourceId.eq(sourceId)
+//					.and(QUser.user.dataSourceAggregate.sources.any().datasetName.eq(dataSetName));
+//		}
+//	}
 }
