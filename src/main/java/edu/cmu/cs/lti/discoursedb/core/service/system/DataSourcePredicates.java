ecdd797deec14af664d19167223f7613643408a6
ferschke <ferschke@cs.cmu.edu>
2015-10-28 16:23:02 -0400

    Added null check to DataSourcePredicates

--- src/main/java/edu/cmu/cs/lti/discoursedb/core/service/system/DataSourcePredicates.java
+++ src/main/java/edu/cmu/cs/lti/discoursedb/core/service/system/DataSourcePredicates.java
@@ -11,21 +11,36 @@ public final class DataSourcePredicates {
 	private DataSourcePredicates() {}
 
 	public static BooleanExpression hasSourceId(String entitySourceId) {
+		if(entitySourceId==null||entitySourceId.isEmpty()){
+			return QDataSourceInstance.dataSourceInstance.isNull();
+		}
 		return QDataSourceInstance.dataSourceInstance.entitySourceId.eq(entitySourceId);
 	}
 	public static BooleanExpression hasSourceType(DataSourceTypes type) {
+		if(type==null){
+			return QDataSourceInstance.dataSourceInstance.isNull();
+		}
 		return QDataSourceInstance.dataSourceInstance.sourceType.eq(type);
 	}
 	
 	public static BooleanExpression hasAssignedEntity(DataSources entitySourceAggregate) {
+		if(entitySourceAggregate==null){
+			return QDataSourceInstance.dataSourceInstance.isNull();
+		}
 		return QDataSourceInstance.dataSourceInstance.sourceAggregate.id.eq(entitySourceAggregate.getId());
 	}
 	
 	public static BooleanExpression hasDataSetName(String dataSetName) {
+		if(dataSetName==null||dataSetName.isEmpty()){
+			return QDataSourceInstance.dataSourceInstance.isNull();
+		}
 		return QDataSourceInstance.dataSourceInstance.datasetName.eq(dataSetName);
 	}
 	
 	public static BooleanExpression hasEntitySourceDescriptor(String entitySourceDescriptor) {
+		if(entitySourceDescriptor==null||entitySourceDescriptor.isEmpty()){
+			return QDataSourceInstance.dataSourceInstance.isNull();
+		}
 		return QDataSourceInstance.dataSourceInstance.entitySourceDescriptor.eq(entitySourceDescriptor);
 	}
 }
