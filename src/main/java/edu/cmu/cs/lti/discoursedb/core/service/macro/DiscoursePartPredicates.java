f0bdee1b16f1b3e26de57434b2e2e2b7beb7962b
ferschke <ferschke@cs.cmu.edu>
2016-01-14 15:27:15 -0500

    Added DiscoursePart retrieval method based on data sources

--- src/main/java/edu/cmu/cs/lti/discoursedb/core/service/macro/DiscoursePartPredicates.java
+++ src/main/java/edu/cmu/cs/lti/discoursedb/core/service/macro/DiscoursePartPredicates.java
@@ -4,7 +4,9 @@ import com.mysema.query.types.expr.BooleanExpression;
 
 import edu.cmu.cs.lti.discoursedb.core.model.macro.Discourse;
 import edu.cmu.cs.lti.discoursedb.core.model.macro.DiscoursePartType;
+import edu.cmu.cs.lti.discoursedb.core.model.macro.QContribution;
 import edu.cmu.cs.lti.discoursedb.core.model.macro.QDiscoursePart;
+import edu.cmu.cs.lti.discoursedb.core.model.system.DataSourceInstance;
 
 public final class DiscoursePartPredicates {
 
@@ -20,7 +22,7 @@ public final class DiscoursePartPredicates {
 	}
 
 	public static BooleanExpression discoursePartHasName(String name) {
-		if (name == null||name.isEmpty()) {
+		if (name == null || name.isEmpty()) {
 			return QDiscoursePart.discoursePart.isNull();
 		} else {
 			return QDiscoursePart.discoursePart.name.eq(name);
@@ -32,6 +34,14 @@ public final class DiscoursePartPredicates {
 			return QDiscoursePart.discoursePart.isNull();
 		} else {
 			return QDiscoursePart.discoursePart.type.eq(type);
+		}
+	}
+	
+	public static BooleanExpression discoursePartHasDataSource(DataSourceInstance dataSource) {
+		if (dataSource == null) {
+			return QDiscoursePart.discoursePart.isNull();
+		} else {
+			return QDiscoursePart.discoursePart.dataSourceAggregate.sources.contains(dataSource);
 		}
 	}
 
