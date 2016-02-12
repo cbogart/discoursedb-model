7f97dd3cd522a7cad6a351b0e302ed16b884db3b
ferschke <ferschke@cs.cmu.edu>
2016-02-12 12:01:49 -0500

    Fixed resource name of rest endpoint of Annotations entity

--- src/main/java/edu/cmu/cs/lti/discoursedb/core/repository/annotation/AnnotationsRepository.java
+++ src/main/java/edu/cmu/cs/lti/discoursedb/core/repository/annotation/AnnotationsRepository.java
@@ -1,8 +1,11 @@
 package edu.cmu.cs.lti.discoursedb.core.repository.annotation;
 
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import edu.cmu.cs.lti.discoursedb.core.model.annotation.Annotations;
 import edu.cmu.cs.lti.discoursedb.core.repository.CoreBaseRepository;
 
+@RepositoryRestResource(collectionResourceRel = "annotations", path = "annotations")
 public interface AnnotationsRepository extends CoreBaseRepository<Annotations,Long>{
     
     
